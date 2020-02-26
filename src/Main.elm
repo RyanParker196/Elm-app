3521d895618113aef9e10bb72217492da42d82db
RyanParker196 <46787914+RyanParker196@users.noreply.github.com>
2020-02-26 15:22:59 -0500

    test

--- src/Main.elm
+++ src/Main.elm
@@ -4,25 +4,19 @@ import Browser
 import Html exposing (..)
 import Html.Attributes exposing (src)
 
-
 ---- MODEL ----
-
 
 type alias Model =
     {}
-
 
 init : ( Model, Cmd Msg )
 init =
     ( {}, Cmd.none )
 
-
-
 ---- UPDATE ----
 
 type Msg
     = NoOp
-
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
@@ -40,25 +34,12 @@ view model =
 
 
 
+    
+
 ---- PROGRAM ----
 
 
-main : Program () Model Msg
 main =
-    div []
-    [ h1 [] [ text "My Grocery List" ]
-    , ul []
-        [ li [] [ text "Black Beans" ]
-        , li [] [ text "Limes" ]
-        , li [] [ text "Greek Yogurt" ]
-        , li [] [ text "Cilantro" ]
-        , li [] [ text "Honey" ]
-        , li [] [ text "Sweet Potatoes" ]
-        , li [] [ text "Cumin" ]
-        , li [] [ text "Chili Powder" ]
-        , li [] [ text "Quinoa" ]
-        ]
-    ]
     Browser.element
         { view = view
         , init = \_ -> init
